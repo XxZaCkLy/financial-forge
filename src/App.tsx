73cd06008489a191ee488dac479f4171961a4ae6
XxZaCkLy <xzackly55@gmail.com>
2025-09-27 15:16:41 +0000

    92725

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 // src/App.tsx
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import HomeScreen from "./screens/HomeScreen";
-import DebtsScreen from "./screens/DebtsScreen";
-import AddDebtScreen from "./screens/AddDebtScreen";
-import SettingsScreen from "./screens/SettingsScreen";
-import { DebtProvider } from "./context/DebtContext";
-
-const Stack = createNativeStackNavigator();
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <DebtProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Debts" component={DebtsScreen} />
-          <Stack.Screen name="AddDebt" component={AddDebtScreen} />
-          <Stack.Screen name="Settings" component={SettingsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </DebtProvider>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20 }}>✅ Expo + TypeScript is working!</Text>
+    </View>
   );
 }
