cf9ffc08d77a42554fc67493f493ac0561759ce0
XxZaCkLy <xzackly55@gmail.com>
2025-09-27 15:41:46 +0000

    v0.1.02

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-// src/App.tsx
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import HomeScreen from "./screens/HomeScreen";
-import SettingsScreen from "./screens/SettingsScreen";
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+<Stack.Navigator initialRouteName="Home">
+  <Stack.Screen name="Home" component={HomeScreen} />
+  <Stack.Screen name="Settings" component={SettingsScreen} />
+  <Stack.Screen name="Dashboard" component={DashboardScreen} />
+</Stack.Navigator>
